88dfd26c9e12ba8594ee94a5ac0c7c025bce4384
Vishrut <vishrut@compoundeye.com>
2021-06-09 13:11:04 -0700

    adding debug list

--- process_movies.sh
+++ process_movies.sh
@@ -9,15 +9,18 @@ do
     num_chapters=`ls image_left/$movie | wc -l`
     for chapter in $(seq 1 $num_chapters)
     do
-	echo "processing chapter $chapter"
+	echo "processing $movie chapter $chapter"
 	$PYTHON reconstructionFlow.py --movie=$movie --chapter=$chapter --ce_repo=$ce_home
     done
 done < "movie_names.txt"
 
 $PYTHON get_disp_and_uncertainty.py lists/train.txt
-$PYTHON python3 get_disp_and_uncertainty.py lists/validation.txt
-$PYTHON python3 get_disp_and_uncertainty.py lists/test.txt
+$PYTHON get_disp_and_uncertainty.py lists/validation.txt
+$PYTHON get_disp_and_uncertainty.py lists/test.txt
 
 $PYTHON create_dataset_csv.py lists/train.txt
 $PYTHON create_dataset_csv.py lists/validation.txt
 $PYTHON create_dataset_csv.py lists/test.txt
+
+# $PYTHON get_disp_and_uncertainty.py lists/debug.txt
+# $PYTHON create_dataset_csv.py lists/debug.txt lists/debug.csv
